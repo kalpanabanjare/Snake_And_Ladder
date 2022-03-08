ae42ef20b76bdeb01fe098ad16a50401a33f28b4
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-03-08 18:57:49 +0530

    Added: Starting Position at 0

--- Snake_And_Ladder/Program.cs
+++ Snake_And_Ladder/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Welcome to Snake and Ladder game");
+﻿using System;
+
+namespace Snake_And_Ladder
+{
+    class SnakeAndLadder
+    {
+        static void Main(String[] args)
+        {
+            int Start_Player_Position = 0;
+            Console.WriteLine("Welcome to Snake and Ladder game");
+            Console.WriteLine("Position of Player: " + Start_Player_Position);
+        }
+    }
+}
+

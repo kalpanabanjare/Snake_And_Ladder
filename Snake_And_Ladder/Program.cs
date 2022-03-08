fe6a97a4f9e3ce01e3e39f571514dd39bfa36706
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-03-08 19:22:00 +0530

    Added: UC2 Roll the Dice

--- Snake_And_Ladder/Program.cs
+++ Snake_And_Ladder/Program.cs
@@ -7,8 +7,11 @@ namespace Snake_And_Ladder
         static void Main(String[] args)
         {
             int Start_Player_Position = 0;
+            Random random = new Random();
+            int DiceRoll = random.Next(1, 7);
             Console.WriteLine("Welcome to Snake and Ladder game");
             Console.WriteLine("Position of Player: " + Start_Player_Position);
+            Console.WriteLine("Roll the Dice: " + DiceRoll);
         }
     }
 }

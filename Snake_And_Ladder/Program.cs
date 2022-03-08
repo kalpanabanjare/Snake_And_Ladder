d99127835bca78b84ce4ea09f10f6db33ff8e604
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-03-08 20:32:10 +0530

    Added: UC3 to check No Play, Ladder or Snake

--- Snake_And_Ladder/Program.cs
+++ Snake_And_Ladder/Program.cs
@@ -4,14 +4,32 @@ namespace Snake_And_Ladder
 {
     class SnakeAndLadder
     {
+        public const int Start_Player_Position = 0;
+        public const int No_Play = 0;
+        public const int Ladder = 1;
+        public const int Snake = 2;
         static void Main(String[] args)
         {
-            int Start_Player_Position = 0;
+            int Position = 0;
             Random random = new Random();
             int DiceRoll = random.Next(1, 7);
+            Random check = new Random();
+            int NoPlayLadderSnake = random.Next(0, 3);
+            switch (NoPlayLadderSnake)
+            {
+                case No_Play:
+                    break;
+                case Ladder:
+                    Position++;
+                    break;
+                case Snake:
+                    Position--;
+                    break;
+            }
             Console.WriteLine("Welcome to Snake and Ladder game");
             Console.WriteLine("Position of Player: " + Start_Player_Position);
             Console.WriteLine("Roll the Dice: " + DiceRoll);
+            Console.WriteLine("Position of Player: " + Position);
         }
     }
 }
